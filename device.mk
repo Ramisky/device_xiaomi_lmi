70cf5214975367fdca4df51b3f5e50057bc5bd0a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-14 14:16:45 +0100

    lmi: Declare notification LED capabilities
    Change-Id: I89dab65991abcce2bcad52d65bdeb31fa637f334

--- device.mk
+++ device.mk
@@ -6,7 +6,8 @@
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-aosp
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
