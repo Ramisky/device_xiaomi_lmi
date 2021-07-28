d769566ec8179cdc0592d8f090311fa1b7799a65
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-28 17:44:40 +0200

    lmi: Decommonize barometer sensor feature permission
    Change-Id: Id5df3ea76b56180a79ebf295356e64f34ff2acc9
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -32,6 +32,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.motor@1.0.vendor
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml
+
 # Inherit from sm8250-common
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
