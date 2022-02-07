59d9db4ef4a4e872e478b8d5b04c114934c518e1
TheScarastic <warabhishek@gmail.com>
2022-02-07 08:58:45 +0530

    lmi: parts: Adapt to S style
     * Use Theme.SubSettingsBase for theme
     * Replace PreferenceActivity with CollapsingToolbarBaseActivity
     * Add Titles to preference screens
     * Remove onOptionsItemSelected and where neccessary move to activity
    
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- parts/src/org/lineageos/devicesettings/dcdimming/DCDimmingSettingsActivity.java
+++ parts/src/org/lineageos/devicesettings/dcdimming/DCDimmingSettingsActivity.java
@@ -19,9 +19,11 @@
 package org.lineageos.devicesettings.dcdimming;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 
-public class DCDimmingSettingsActivity extends PreferenceActivity {
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class DCDimmingSettingsActivity extends CollapsingToolbarBaseActivity {
 
     private static final String TAG_HTSR = "dcdimming";
 
@@ -29,7 +31,7 @@ public class DCDimmingSettingsActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(android.R.id.content,
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
                 new DCDimmingSettingsFragment(), TAG_HTSR).commit();
     }
 }
